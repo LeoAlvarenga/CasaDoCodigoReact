2b4ce803ceaa4ebd11e5d0e7f015042e1ce98ba6
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-04-08 17:46:07 -0300

    Passando propriedades para o componente

--- src/App.js
+++ src/App.js
@@ -3,9 +3,36 @@ import './App.css';
 import Tabela from "./Tabela";
 
 function App() {
+  const autores = [
+    {
+      nome: 'Paulo',
+      livro: 'React',
+      preco: '1000'
+    },
+    {
+      nome: 'Daniel',
+      livro: 'Java',
+      preco: '99'
+    },
+    {
+      nome: 'Marcos',
+      livro: 'Design',
+      preco: '150'
+    },
+    {
+      nome: 'Bruno',
+      livro: 'DevOps',
+      preco: '100'
+    },
+    {
+      nome: 'Nico',
+      livro: 'Java',
+      preco: '9999'
+    }
+  ];
   return (
     <div className="App">
-      <Tabela/>
+      <Tabela autores={autores}/>
     </div>
   );
 }
--- src/Tabela.js
+++ src/Tabela.js
@@ -12,27 +12,23 @@ const TableHead = () => {
         </thead>
     );
 }
-const TableBody = () => {
+const TableBody = props => {
+    
+    const linhas = props.autores.map((linha, index) => {
+
+        return (
+            <tr key={index}>
+                <td>{linha.nome}</td>
+                <td>{linha.livro}</td>
+                <td>{linha.preco}</td>
+                <td><button>Remover</button></td>
+            </tr>
+        );
+    });
+    
     return (
         <tbody>
-            <tr>
-                <td>Paulo</td>
-                <td>React</td>
-                <td>1000</td>
-                <td><button>Remover</button></td>
-            </tr>
-            <tr>
-                <td>Nico</td>
-                <td>React</td>
-                <td>1000</td>
-                <td><button>Remover</button></td>
-            </tr>
-            <tr>
-                <td>Daniel</td>
-                <td>React</td>
-                <td>1000</td>
-                <td><button>Remover</button></td>
-            </tr>
+            {linhas}
         </tbody>
     );
 }
@@ -40,10 +36,13 @@ const TableBody = () => {
 class Tabela extends Component {
 
     render() {
+
+        const { autores } = this.props;
+
         return (
             <table>
                 <TableHead />
-                <TableBody/>
+                <TableBody autores = { autores } />
             </table>
         )
     }
