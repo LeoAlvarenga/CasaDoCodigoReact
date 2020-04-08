21333a5673751b8386f43d121094d1d738adebc1
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-04-08 18:30:38 -0300

    Adicionando Formulário

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Tabela from "./Tabela";
+import Form from "./Fomulario";
 
 class App extends Component {
 
   state = {
-    autores : [
+    autores: [
       {
         nome: 'Paulo',
         livro: 'React',
@@ -48,15 +49,20 @@ class App extends Component {
     )
 
   }
-  
-  render(){
+
+  escutadorDeSubmit = autor => {
+    this.setState({autores: [...this.state.autores, autor]})
+  }
+
+  render() {
 
     return (
-      <div className="App">
-        <Tabela autores={this.state.autores} removeAutor = {this.removeAutor} />
-      </div>
+      <Fragment>
+        <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+        <Form escutadorDeSubmit={this.escutadorDeSubmit} />
+      </Fragment>
     );
-    
+
   }
 
 }
