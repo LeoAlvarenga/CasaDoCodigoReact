bedd98b2a26a713061c226aabcceb82a577d650b
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-04-11 12:14:55 -0300

    Alterando a estilização da tabela

--- src/Components/Tabela/Tabela.js
+++ src/Components/Tabela/Tabela.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import Button from '@material-ui/core/Button'
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 
 
 
--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -1,12 +1,25 @@
 import React, { Fragment } from 'react';
 import Header from '../../Components/Header/Header';
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import {MakeStyles, makeStyles} from '@material-ui/core/styles'
+
+const useEstilos = makeStyles({
+    titulo: {
+        textAlign: 'center',
+        color: '#7986cb'
+    }
+});
 
 const Sobre = () => {
-
+    const classes = useEstilos();
     return (
         <Fragment>
             <Header></Header>
-            <h1>Sobre</h1>
+            <Container maxWidth='sm' >
+            <Typography className={classes.titulo} variant='h1' component='h2' >Sobre</Typography>
+            <Typography variant='body1' component='p' >A Casa do código é uma editora que produz e edita livros em diversos Formatos.</Typography>
+            </Container>
         </Fragment>
     )
 }
